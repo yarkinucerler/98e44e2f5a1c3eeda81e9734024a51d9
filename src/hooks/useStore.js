44ed6ee563c905da6b09dcc5dfb88f1f6f46ea0b
Yarkın ÜÇERLER <yarkin.ucerler@cimri.com>
2024-03-29 16:36:25 +0300

    deleted file

--- src/hooks/useStore.js
+++
@@ -1,6 +0,0 @@
-import { useMemo } from 'react';
-import {makeStore} from "../store/store";
-
-export function useStore(initialState) {
-  return useMemo(() => makeStore(initialState), [initialState]);
-}
